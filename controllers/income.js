ebf2f4cb75a28fa8a1354f168ef4ac21cd2646c7
prince-konwea <kokodecontee123@gmail.com>
2023-07-08 12:42:54 +0100

    adds delete function

--- controllers/income.js
+++ controllers/income.js
@@ -41,3 +41,10 @@ exports.getIncomes = async (req, res) => {
   }
 
 }
+
+
+exports.deleteIncome = async (req, res) => {
+    const {id} = req.params
+    IncomeSchema.findByIdAndDelete(id) 
+    .then((income) => res.status(200).json({message: "Income Deleted"}))
+}

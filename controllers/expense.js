8fcbab155decb2d521bfc35b12b080cbfbb675eb
prince-konwea <kokodecontee123@gmail.com>
2023-07-09 16:16:25 +0100

    create add expense functions

---
+++ controllers/expense.js
@@ -0,0 +1,30 @@
+const ExpenseSchema = require("../models/expenseModel")
+
+
+
+exports.addExpenses = async (req, res) => {
+    const {title, amount, description, category, date} = req.body;
+ 
+     const expense = ExpenseSchema({
+         title,
+         amount,
+         description,
+         category, 
+         date
+     })  
+
+    try{
+        // validation 
+        if(!title || !description || !category)
+            res.status(400).json({message: "All field is required."})
+        
+        if(amount <= 0 || !amount === "number")
+            res.status(400).json({message: "Amount must be a positive number."})
+        expense.save()   
+         res.status(200).json({message: "expense saved successfully."})
+
+    }catch(error){
+      res.status(500).json({message: "Error saving expense"})
+    }
+
+}
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,9 +1,13 @@
 const { addIncome, getIncomes, deleteIncome } = require("../controllers/income");
+const {addExpenses} = require("../controllers/expense")
 
 const router = require("express").Router()
 
 router.post("/add-income", addIncome)
 router.get("/get-incomes", getIncomes)
 router.delete("/delete-income/:id", deleteIncome)
+
+// expenses
+router.post("/add-expenses", addExpenses)
 
 module.exports = router;
